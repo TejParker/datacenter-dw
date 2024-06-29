4e440306f9e5fb618074e4c50f551dc787010952
TejParker <laplace1015@outlook.com>
2024-06-29 12:43:46 +0800

    项目以分模块的形式在开发分支dev进行初始化

---
+++ bin/spark2mysql2.sh
@@ -0,0 +1,15 @@
+BIN_HOME=`cd $(dirname "$0");pwd`
+APP_HOME=`cd ${BIN_HOME}/..;pwd`
+COMMON_LIB="${APP_HOME}/lib/common"
+CORE_LIB="${APP_HOME}/lib/core"
+
+mainJar="${COMMON_LIB}/lib/dx-core-1.0-SNAPSHOT.jar"
+
+libJars=`ls ${APP_HOME}/lib/common/*.jar ${CORE_LIB}/lib/*.jar | awk '{print $1}' | tr "\n" "," | sed 's/.$//'`
+
+spark-submit \
+--master local[*] \
+--deploy-mode client \
+--class com.ziyun.datacenter.mysql2hdfs.Mysql2Hdfs \
+--jars ${libJars} \
+${mainJar}
